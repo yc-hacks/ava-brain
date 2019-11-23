9a566eb8d23b8b3eaccb3c7b2737fc10ebd83788
Kevin Hou <kevin.ch.hou@gmail.com>
2019-11-22 23:32:59 -0800

    Working flask server

--- application.py
+++ application.py
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run(port=6000)
+    application.run(host="0.0.0.0", port=80)
