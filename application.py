4594d1627f5610763fe0fa8b00a2cd2775655222
Kevin Hou <kevin.ch.hou@gmail.com>
2019-11-23 03:08:30 -0800

    Change

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from qa import Ava
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
-CORS(application)
+cors = CORS(app, origins=["http://localhost:3000"], headers=['Content-Type'], expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
 
 ava = Ava()
 
